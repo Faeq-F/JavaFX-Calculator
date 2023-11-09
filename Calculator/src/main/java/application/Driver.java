900eaa5abcd32e51f3db654e710a2a79dde4c47e
zlac318 <zlac318@live.rhul.ac.uk>
2023-11-09 16:45:15 +0000

    Added the classes from the initial project provided into organized namespaces.

---
+++ Calculator/src/main/java/application/Driver.java
@@ -0,0 +1,34 @@
+package application;
+
+import application.calculator.CalcModel;
+import application.view.AsciiView;
+import application.view.CalcController;
+import application.view.CalcView;
+import application.view.ViewInterface;
+
+/**
+ * A driver class that sets up the MVC framework and begins the application.
+ */
+public class Driver {
+
+  /**
+   * The entry point for the calculator.
+   * 
+   * @param args ignored - could be used to choose which view to load in future?
+   */
+  public static void main(String[] args) {
+    ViewInterface view = new AsciiView();
+
+    CalcModel model = new CalcModel();
+    System.out.println("hello");
+
+    // Decide which view to build.
+    if (System.console() == null) {
+      System.out.println("hello");
+      view = CalcView.getInstance();
+    }
+    new CalcController(model, view);
+    // All ready so begin the interface.
+    view.startView();
+  }
+}
