5d04a53a74a3e4d4c75d4225d7c63596b402db36
zlac318 <zlac318@live.rhul.ac.uk>
2023-11-28 09:14:32 +0000

    Completed test 91 & 92; Testing larger mathematical expressions with CalcModel & CalcController.

--- Calculator/src/test/java/application/calculator/TestCalcModel.java
+++ Calculator/src/test/java/application/calculator/TestCalcModel.java
@@ -29,5 +29,26 @@ class TestCalcModel {
     assertDoesNotThrow(() -> assertEquals(firstNum * secondNum,
         model.evaluate(firstNum + " " + secondNum + " *")));
   }
+  
+  @Test
+  // Test 91
+  // Testing larger mathematical expressions with default postfix calc
+  void testLargeExpressions() {
+    assertDoesNotThrow(() -> assertEquals(63, model.evaluate("5 6 7 + * 2 -")));
+
+    assertDoesNotThrow(
+        () -> assertEquals(21.5454, model.evaluate("10 6 9 3 + -11 * / * 17 + 5 +"), 0.002));
+
+    assertDoesNotThrow(() -> assertEquals(1357.3636,
+        model.evaluate("5 6 7 + * 2 - 10 6 9 3 + -11 * / * 17 + 5 + *"), 0.002));
+
+    assertDoesNotThrow(
+        () -> assertEquals(29308.02, model.evaluate("5 6 7 + * 2 - 10 6 9 3 + -11 * / * 17 + 5 + *"
+            + " 10 6 9 3 + -11 * / * 17 + 5 + * 5 6 7 + * 2 - +"), 0.02));
+
+    assertDoesNotThrow(() -> assertEquals(1420.3636,
+        model.evaluate("5 6 7 + * 2 - 10 6 9 3 + -11 * / * 17 + 5 + * 5 6 7 + * 2 - +"),
+        0.002));
+  }
 
 }
